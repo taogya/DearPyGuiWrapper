161e8f4e6f39d691056e8b6009fa0443a23e783d
Taogya <132679709+taogya@users.noreply.github.com>
2025-03-21 03:20:00 +0900

    fix bug

--- dearpygui_wrapper/__init__.py
+++ dearpygui_wrapper/__init__.py
@@ -1,10 +1,11 @@
 import dearpygui.dearpygui as dpg
-from dearpygui_wrapper.node_editor import (Node, NodeAttribute,
-                                           NodeAttributeType, NodeEditor)
-from dearpygui_wrapper.value import InputText, Text
-from dearpygui_wrapper.window import ViewPort, Window
 
 DpgTag = int | str
+
+from dearpygui_wrapper.node_editor import (Node, NodeAttribute,  # noqa: E402
+                                           NodeAttributeType, NodeEditor)
+from dearpygui_wrapper.value import InputText, Text  # noqa: E402
+from dearpygui_wrapper.window import ViewPort, Window  # noqa: E402
 
 __all__ = [
     'dpg',
